83ce370d0d1a920d9b4e7cce2b9324b21330a108
Dawid Stasiak <dawid.stasiak21@gmail.com>
2024-03-02 15:58:29 +0100

    Create project

---
+++ src/main/java/xyz/stasiak/takemoviestore/TakeMovieStoreApplication.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.takemoviestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeMovieStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeMovieStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/stasiak/takemoviestore/TakeMovieStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.takemoviestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeMovieStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
